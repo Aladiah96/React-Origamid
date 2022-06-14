3a12b66106fb630b3cbea56b274890b3a26a9278
Aladiah <wagner.aladiah@gmail.com>
2022-06-14 13:44:29 -0300

    Continuing the React course.

---
+++ reactapp/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <a className="ativo" href="https://origamid.com" title="Isso é um site">
+      Origamid
+    </a>
+  );
+};
+
+export default App;
