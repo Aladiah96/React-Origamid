19ec8126add5c90c79b585b0b88cfc6f4400bc2c
Aladiah <wagner.aladiah@gmail.com>
2022-07-28 08:26:48 -0300

    Added few pages to the project

--- final-project/dogs/src/App.js
+++ final-project/dogs/src/App.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import './App.css'
+
+
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>React app</div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
