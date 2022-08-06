d5394197457dc7b40e50cf650fd95b69da9bd179
aladiah <wagner.aladiah@gmail.com>
2022-08-06 18:37:00 -0300

    Create and stylize most of the login page

--- final-project/dogs/src/App.js
+++ final-project/dogs/src/App.js
@@ -6,6 +6,7 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -13,12 +14,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login/*' element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login/*' element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </>
   );
