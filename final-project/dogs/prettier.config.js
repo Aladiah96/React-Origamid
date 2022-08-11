b1fefb516f0047a76bd0f4fcd5654d234cd8f470
aladiah <wagner.aladiah@gmail.com>
2022-08-11 00:37:21 -0300

    Implement several new features to the final project

---
+++ final-project/dogs/prettier.config.js
@@ -0,0 +1,12 @@
+const options = {
+  arrowParens: 'avoid',
+  singleQuoet: true,
+  bracketSpacing: true,
+  endOfline: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
+
+module.exports = options
+
